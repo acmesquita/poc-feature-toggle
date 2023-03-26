7c4f07819096b68690cef4ba8656b76eb30ae4cd
Catharina <catha.ana.1994@gmail.com>
2023-03-26 13:58:29 -0300

    test: add feature resource

--- test/models/feature_resource_test.rb
+++ test/models/feature_resource_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
 
 class FeatureResourceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create new resource valid" do
+    feature = Feature.new(flag: 'new_page')
+    resource = Resource.new(uid: '123')
+
+    sut = FeatureResource.new(
+      feature: feature,
+      resource: resource
+    )
+
+    assert sut.value
+    assert sut.save
+  end
+
+  test "no create feature resource with invalid params" do
+    assert_raises(ActiveRecord::RecordInvalid) do
+      sut = FeatureResource.new(
+        feature: nil,
+        resource: nil
+      )
+      sut.save!
+    end
+  end
 end
