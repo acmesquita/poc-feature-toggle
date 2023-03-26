d8c7cabb343eccd9013629848fcf6a4db12235bb
Catharina <catha.ana.1994@gmail.com>
2023-03-26 13:54:40 -0300

    test: add featuretest

--- test/models/feature_test.rb
+++ test/models/feature_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class FeatureTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create new feature valid" do
+    sut = Feature.new(flag: 'new_page')
+
+    assert sut.save
+  end
+
+  test "no create feature with invalid params" do
+    assert_raises(ActiveRecord::RecordInvalid) do
+      sut = Feature.new()
+      sut.save!
+    end
+  end
 end
