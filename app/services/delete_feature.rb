4f5c5ddaea5944ab92b018010f16cf8aa2de9486
Catharina Mesquita <catha.ana.1994@gmail.com>
2023-04-06 09:41:42 -0300

    feat: add validation to delete a feature

--- app/services/delete_feature.rb
+++ app/services/delete_feature.rb
@@ -1,5 +1,17 @@
-class DeleteFeature
+class DeleteFeature < BaseService
+  validates_presence_of :id
+
+  attr_reader :id
+
   def self.perfom(params)
-    FeatureRepository.destroy(params[:id])
+    new(params).perfom
+  end
+
+  def initialize(params)
+    @id = params[:id]
+  end
+
+  def perfom
+    FeatureRepository.destroy(@id)
   end
 end
